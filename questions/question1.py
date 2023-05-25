b8db9f01b357b943ac1e93d5e28bc64b7e279851
sxing <1557177832@qq.com>
2023-05-25 11:37:41 +0800

    q1已完成

--- questions/question1.py
+++ questions/question1.py
@@ -1,35 +1,42 @@
 # 1. 判断三角形类型（用边界值分析和设计测试用例）。
+import numpy as np
+
 
 def triangle_atom(a, b, c):
     if (a <= 0 or a > 800) or (b <= 0 or b > 800) or (c <= 0 or c > 800):
-        return 'out of range'
+        return 'Out of range'
     if a + b > c and a + c > b and b + c > a:
         if a == b or a == c or b == c:
             if a == b and b == c:
-                return 'equilateral triangle'
+                return 'Equilateral triangle'
             else:
-                return 'isosceles triangle'
+                return 'Isosceles triangle'
         else:
-            return 'normal triangle'
+            return 'Normal triangle'
     else:
-        return 'not a triangle'
+        return 'Not a triangle'
 
 
 def manual_test():
     print("question 1 manual test")
     print("Please enter the lengths of the three sides, for example 1 1 1")
     while True:
-        a = int(input())
-        b = int(input())
-        c = int(input())
+        nums = input("请输入三个数字，以空格分隔：")
+        a, b, c = map(float, nums.split())
         print(triangle_atom(a, b, c))
 
-    # 手动测试的函数实现
+    # 手动测试的函数实现1
     pass
 
 
 def auto_test():
+    data = np.loadtxt(open("testData/triangle_equivalenceclass.csv"), delimiter=",", usecols=[1, 2, 3])
+    #data_t = np.loadtxt(open("testData/triangle_equivalenceclass.csv"), delimiter=",", usecols=[4])
     print("question 1 auto test")
+    for data_row in data:
+        print("本次自动测试的数据为 %d %d %d" % (data_row[0], data_row[1], data_row[2]))
+        print("输出结果为：" + triangle_atom(data_row[0], data_row[1], data_row[2]))
+        #print("解释说明：" + data_row[3])
     # 自动测试的函数实现
     pass
 
